cbb65831efe2e077c43c4e8f40645ab73322c0e5
Ola <nassima.benkraouche@gmail.com>
2019-11-27 14:29:13 +0100

    Create README.js

---
+++ CDN/README.js
@@ -0,0 +1,11 @@
+## HLS streaming from node
+Generate your .m3u8 playlist using tools in the /Converting directory and place them in catalogue directory.
+This node server will serve up the m3u8 files to an HLS compatible client (Chrome).  
+
+## Using
+
+node server.js
+
+ * Access localhost:PORT/playlist_name.m3u8 to get the file.
+ * Access localhost:PORT/list to get the list of the m3u8 playlists availble in catalogue (to be improved/ parse m3u8 extensions only).
+ 
