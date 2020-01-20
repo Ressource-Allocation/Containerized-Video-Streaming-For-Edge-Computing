760db498ba05f0199ed3bbe2de492b20bd53a471
Artogg <40455433+Artogg@users.noreply.github.com>
2020-01-20 22:40:58 +0100

    Update request.sh

--- CDN/tests/request.sh
+++ CDN/tests/request.sh
@@ -2,7 +2,7 @@
 RequestNumber=5
 for ((i=1;i <= $RequestNumber; i++))
 do
-	(google-chrome --user-data-dir="$HOME/$i" --disable-web-security --disable-gpu --new-window 127.0.0.1) &
+	(google-chrome --user-data-dir="$HOME/DevSession" --disable-web-security --disable-gpu --new-window 127.0.0.1) &
 done
 sleep 30 
 killall -9 /opt/google/chrome/chrome
